262d444164a110d4b9d58322a3c71ab65d2efe14
temkarus0070 <pretkovartyom@gmail.com>
2022-01-19 20:28:11 +0300

    init

---
+++ src/main/java/org/netcracker/eventteammatessearch/EventTeammatesSearchApplication.java
@@ -0,0 +1,13 @@
+package org.netcracker.eventteammatessearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTeammatesSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTeammatesSearchApplication.class, args);
+    }
+
+}
