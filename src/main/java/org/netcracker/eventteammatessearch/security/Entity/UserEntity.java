024407a84d665867ebce220c31bfd511c657a0ef
artyom <pretkovartyom@gmail.com>
2022-03-11 22:58:54 +0300

    try to add auth

---
+++ src/main/java/org/netcracker/eventteammatessearch/security/Entity/UserEntity.java
@@ -0,0 +1,19 @@
+package org.netcracker.eventteammatessearch.security.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserEntity {
+    private String username;
+    private String password;
+    private
+}
---
+++ src/main/java/org/netcracker/eventteammatessearch/security/RegistrationController.java
@@ -0,0 +1,13 @@
+package org.netcracker.eventteammatessearch.security;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+    public void register(String username, String login) {
+
+
+    }
+
+
+}
