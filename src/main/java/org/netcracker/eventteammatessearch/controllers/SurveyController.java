71a00b202f09487b4358f60df93faeabff43c2bb
Dmtr1i <dmitry1klimenko@mail.ru>
2022-03-21 16:44:42 +0300

    realise SurveyController

--- src/main/java/org/netcracker/eventteammatessearch/controllers/SurveyController.java
+++ src/main/java/org/netcracker/eventteammatessearch/controllers/SurveyController.java
@@ -19,20 +19,17 @@ public class SurveyController {
         user.setLogin(name);
         data.setUser(user);
         user.setSurveyResult(data);
-        /*
+/*
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\nTypes: ");
-        for (int i = 0; i < user.getSurveyResult().getType().size(); i++) System.out.print(user.getSurveyResult().getType().get(i));
+        for (int i = 0; i < user.getSurveyResult().getType().size(); i++) System.out.print(user.getSurveyResult().getType().get(i) + " ");
         System.out.println("\nFormats");
         for (int i = 0; i < user.getSurveyResult().getFormat().size(); i++) System.out.print(user.getSurveyResult().getFormat().get(i));
         System.out.println("date: " + user.getSurveyResult().getDateTimeStart() + " - " + user.getSurveyResult().getDateTimeEnd());
         System.out.println("members: " + user.getSurveyResult().getMinNumberOfGuests() + " - " + user.getSurveyResult().getMaxNumberOfGuests());
         System.out.println("money: " + user.getSurveyResult().getMinPrice() + " - " + user.getSurveyResult().getMaxPrice());
         System.out.println("location: " + user.getSurveyResult().getLocation());
-        System.out.println("tempUser: " + data.getUser());
-        System.out.println("surveyUser" + user);
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
-         */
+*/
         return data;
     }
 }
