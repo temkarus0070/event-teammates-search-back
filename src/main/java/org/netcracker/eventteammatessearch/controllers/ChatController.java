501ce932bec0d7dc9cc811b4384665085cc45fb9
artyom <pretkovartyom@gmail.com>
2022-04-01 00:05:04 +0300

    add websocket support

--- src/main/java/org/netcracker/eventteammatessearch/controllers/ChatController.java
+++ src/main/java/org/netcracker/eventteammatessearch/controllers/ChatController.java
@@ -1,6 +1,7 @@
 package org.netcracker.eventteammatessearch.controllers;
 
 import org.netcracker.eventteammatessearch.entity.mongoDB.Message;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -9,10 +10,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
-    @MessageMapping("/sendMessage")
-    @SendTo("/chat/#message.chatId")
-    public Message sendMessage(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username", message.getUserId());
+    @MessageMapping("/sendMessage/{chatId}")
+    @SendTo("/chat/messages/{chatId}")
+    public Message sendMessage(@DestinationVariable long chatId, @Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
+        // headerAccessor.getSessionAttributes().put("username", message.getUserId());
         return message;
     }
 
