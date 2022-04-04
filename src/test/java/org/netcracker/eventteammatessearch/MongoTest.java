10d4f16897a1db09b4da2e3724ac122f09046ce7
artyom <pretkovartyom@gmail.com>
2022-04-04 13:46:20 +0300

    repair

--- src/test/java/org/netcracker/eventteammatessearch/MongoTest.java
+++ src/test/java/org/netcracker/eventteammatessearch/MongoTest.java
@@ -7,7 +7,12 @@ import org.netcracker.eventteammatessearch.entity.mongoDB.EventLengthMark;
 import org.netcracker.eventteammatessearch.entity.mongoDB.Review;
 import org.netcracker.eventteammatessearch.persistence.repositories.mongo.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
@@ -16,7 +21,8 @@ import java.util.List;
 
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
-
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@Profile("client_app_profile_name")
 public class MongoTest {
 
 
@@ -24,7 +30,6 @@ public class MongoTest {
 
     @DisplayName("test avg calculate")
     public void test() {
-
         reviewRepository.save(new Review(new Review.ReviewId(1, "1"), "1", "", 9,
                 1, 1, 0.5, EventLengthMark.FINE, new HashSet<>()));
         reviewRepository.save(new Review(new Review.ReviewId(1, "2"), "1", "", 9,
