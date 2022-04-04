7ff6252ae1eff1395110f8114aaf885095177d00
artyom <pretkovartyom@gmail.com>
2022-04-04 13:52:23 +0300

    repair

--- src/test/java/org/netcracker/eventteammatessearch/MongoTest.java
+++ src/test/java/org/netcracker/eventteammatessearch/MongoTest.java
@@ -2,18 +2,12 @@ package org.netcracker.eventteammatessearch;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.netcracker.eventteammatessearch.entity.mongoDB.EventLengthMark;
 import org.netcracker.eventteammatessearch.entity.mongoDB.Review;
 import org.netcracker.eventteammatessearch.persistence.repositories.mongo.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
@@ -22,15 +16,12 @@ import java.util.List;
 
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@Profile("client_app_profile_name")
 public class MongoTest {
 
 
     @Autowired  ReviewRepository reviewRepository;
 
     @DisplayName("test avg calculate")
-    @Test
     public void test() {
         reviewRepository.save(new Review(new Review.ReviewId(1, "1"), "1", "", 9,
                 1, 1, 0.5, EventLengthMark.FINE, new HashSet<>()));
