84d6b7d566fafb51ea2582a87cd71a9be8cf292b
artyom <pretkovartyom@gmail.com>
2022-04-29 15:05:15 +0300

    remove expired events from map

--- src/main/java/org/netcracker/eventteammatessearch/persistence/repositories/EventRepository.java
+++ src/main/java/org/netcracker/eventteammatessearch/persistence/repositories/EventRepository.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
 
 
-    @Query(value = "SELECT e from Event  e where distance(e.location.location, :p,true) <= :distanceM")
+    @Query(value = "SELECT e from Event  e where distance(e.location.location, :p,true) <= :distanceM and (e.dateTimeEnd is null or e.dateTimeEnd >   CURRENT_TIMESTAMP)")
     List<Event> findNearWithinDistance(Point p, double distanceM);
 
     @Query(value = "SELECT e FROM Event e inner join e.tags inner join e.guests inner join e.location inner join e.eventType inner join e.owner inner join e.invitedGuests")
