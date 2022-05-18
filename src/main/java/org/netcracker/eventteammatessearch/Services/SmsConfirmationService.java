15c73f0fa4ce3933528c6b9d91991e9c9d9d9f75
artyom <pretkovartyom@gmail.com>
2022-05-18 11:17:35 +0300

    test deploy

--- src/main/java/org/netcracker/eventteammatessearch/Services/SmsConfirmationService.java
+++ src/main/java/org/netcracker/eventteammatessearch/Services/SmsConfirmationService.java
@@ -54,6 +54,19 @@ private static Logger logger=LoggerFactory.getLogger(SmsConfirmationService.clas
 
 
     public void sendToken(Principal principal, String phone) {
+
+        if (true) {
+            if (!phone.contains("+")){
+                phone="+"+phone.trim();
+            }
+            PhoneToken tokenFromServerResponse = new PhoneToken();
+            tokenFromServerResponse.setId(new PhoneTokenKey(LocalDateTime.now(), principal.getName()));
+            tokenFromServerResponse.setCode("88");
+            tokenFromServerResponse.setPhone(phone);
+            phoneTokenService.add(tokenFromServerResponse);
+            return;
+        }
+
         if (!phoneTokenService.checkIfHasNotExpired(principal)) {
           try {
               if (!phone.contains("+")){
