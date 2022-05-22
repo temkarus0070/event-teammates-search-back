6d7cdbb4ca31c44976b806721319a399d770fef9
artyom <pretkovartyom@gmail.com>
2022-05-22 19:53:14 +0300

    repair relationship unique constraints

--- src/main/java/org/netcracker/eventteammatessearch/entity/Relationship.java
+++ src/main/java/org/netcracker/eventteammatessearch/entity/Relationship.java
@@ -1,7 +1,6 @@
 package org.netcracker.eventteammatessearch.entity;
 
 import lombok.*;
-import net.bytebuddy.build.HashCodeAndEqualsPlugin;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
@@ -17,7 +16,6 @@ import java.util.Objects;
 @NamedEntityGraph(name = "relation-graph",includeAllAttributes = true,attributeNodes = {@NamedAttributeNode(value = "owner",subgraph = "user-sub"),
         @NamedAttributeNode(value = "friend",subgraph = "user-sub")}
 ,subgraphs = {@NamedSubgraph(name = "user-sub",attributeNodes = {@NamedAttributeNode("surveyResult")})})
-@EqualsAndHashCode
 public class Relationship {
 
     @MapsId("ownerId")
@@ -44,5 +42,18 @@ public class Relationship {
     public static class RelationshipId implements Serializable {
         private String friendId;
         private String ownerId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Relationship that = (Relationship) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
